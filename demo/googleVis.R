f9805a5e33e17d3af1138472000a2467348d42eb
markus.gesmann <markus.gesmann@83bbe1e1-2e5b-5cd1-b038-f266834e137f>
2010-10-10 21:06:23 +0000

    Cleaned up demo files, added package help file.
    git-svn-id: https://google-motion-charts-with-r.googlecode.com/svn/trunk@61 83bbe1e1-2e5b-5cd1-b038-f266834e137f

---
+++ demo/googleVis.R
@@ -0,0 +1,40 @@
+## googleVis demo
+## A web browse with internet connection and Flash is required
+
+## Motion Chart
+Motion=gvisMotionChart(Fruits, "Fruit", "Year")
+plot(Motion)
+
+## Geo Map
+Geo=gvisGeoMap(Exports[,1:2])
+plot(Geo)
+
+## Table 
+Table=gvisTable(Exports, options=list(width=400, height=300))
+plot(Table)
+
+## TreeMap
+Tree=gvisTreeMap(Regions, options=list(fontSize=16))
+plot(Tree)
+
+## Several charts on one page
+Page <- list(type="MotionGeoTableTree", 
+			 chartid=format(Sys.time(), "MotionGeoTableTree-%Y-%m-%d-%H-%M-%S"), 
+			 html=list(Header=Motion$html$Header,
+			 		Chart1=Motion$html$Chart,
+					Caption1=Motion$html$Caption,
+					Chart2=Geo$html$Chart,
+					Caption2=Geo$html$Caption,
+					Chart3=Table$html$Chart,
+					Caption3=Table$html$Caption,
+					Chart4=Tree$html$Chart,
+					Caption4=Tree$html$Caption,
+					Footer=Tree$html$Footer)
+			)
+		
+
+class(Page) <- list("gvis", class(Page))
+plot(Page)
+
+## See also how googleVis functions can be integrated into rsp-files:
+browseRsp("http://127.0.0.1:8074/library/googleVis/rsp/")
