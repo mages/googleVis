b9bb18dea614285812b4993c5a0842d4896e10a6
markus.gesmann <markus.gesmann@83bbe1e1-2e5b-5cd1-b038-f266834e137f>
2010-12-11 23:48:03 +0000

    Added gadget examples
    git-svn-id: https://google-motion-charts-with-r.googlecode.com/svn/trunk@138 83bbe1e1-2e5b-5cd1-b038-f266834e137f

--- inst/gadgets/gadgets.R
+++ inst/gadgets/gadgets.R
@@ -1,4 +1,5 @@
-Motion=gvisMotionChart(Fruits, idvar="Fruit", timevar="Year")
+
+Motion=gvisMotionChart(Fruits, idvar="Fruit", timevar="Year", options=list(height=350, width=400))
 cat(createGoogleGadget(Motion), file="motionchart.xml")
 
 ## Geo Map with coloured regions
@@ -10,7 +11,7 @@ cat(createGoogleGadget(Geo), file="geomap.xml")
 ## Show Hurricane Andrew (1992) storm track with Geo Map
 AndrewGeo <- gvisGeoMap(Andrew, locationvar="LatLong", numvar="Speed_kt", 
       			 hovervar="Category", 
-      		         options=list(height=350, region="US"))
+      		         options=list(height=250, width=400, region="US"))
 
 cat(createGoogleGadget(AndrewGeo), file="andrewgeomap.xml")
 
@@ -36,12 +37,45 @@ Tree <- gvisTreeMap(Regions,  "Region", "Parent", "Val", "Fac", options=list(fon
 
 cat(createGoogleGadget(Tree), file="treemap.xml")
 
+require(datasets)
+states <- data.frame(state.name, state.area)
+
+states3 <- data.frame(state.region, state.division, state.name, state.area)
+
+regions <- aggregate(list(region.area=states3$state.area),
+                     list(region=state.region), sum)
+
+divisions <- aggregate(list(division.area=states3$state.area),
+                     list(division=state.division, region=state.region),
+                     sum)
+
+my.states3 <- data.frame(regionid=c("USA",
+                                    as.character(regions$region),
+                                    as.character(divisions$division),
+                                    as.character(states3$state.name)),
+                         parentid=c(NA, rep("USA", 4), 
+                                   as.character(divisions$region),
+                                   as.character(states3$state.division)),
+                         state.area=c(sum(states3$state.area),
+                                      regions$region.area,
+                                      divisions$division.area,
+                                      states3$state.area))
+
+my.states3$state.area.log=log(my.states3$state.area)
+
+statesTree3 <- gvisTreeMap(my.states3, "regionid", "parentid",
+                           "state.area", "state.area.log", options=list(showScale=TRUE, width=400, height=300))
+
+cat(createGoogleGadget(statesTree3), file="statestreemap.xml")
+
+
 ## Annotated Time Line Chart
 AnnoTimeLine  <- gvisAnnotatedTimeLine(Stock, datevar="Date",
                            numvar="Value", idvar="Device",
                            titlevar="Title", annotationvar="Annotation",
                            options=list(displayAnnotations=TRUE,
-                            width=600, height=350)
+                             legendPosition='newRow',
+                             width=400, height=250)
                            )
 cat(createGoogleGadget(AnnoTimeLine), file="annotimeline.xml")
 
