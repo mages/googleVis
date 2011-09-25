e8fd0b766a7838f1ec8f0819a84e690dc559244b
markus.gesmann <markus.gesmann@83bbe1e1-2e5b-5cd1-b038-f266834e137f>
2011-09-25 12:07:48 +0000

    Minor editing changes.
    git-svn-id: https://google-motion-charts-with-r.googlecode.com/svn/trunk@287 83bbe1e1-2e5b-5cd1-b038-f266834e137f

--- demo/WorldBank.R
+++ demo/WorldBank.R
@@ -6,24 +6,26 @@
 ## For the World Bank Data terms of use see:
 ## http://data.worldbank.org/summary-terms-of-use
 ##
-## To tun this demo an internet connection and Flash are required.
-## This demo is part of the googleVis R package
+## To run this demo an internet connection and Flash are required.
+## This demo is part of the googleVis R package.
 ##
 ## Markus Gesmann, 24 September 2011
 ## Distributed under GPL 2 or later
 
 
-getWorldBankData <- function(id='SP.POP.TOTL', date='1960:2010', value="value", per.page=12000){
+getWorldBankData <- function(id='SP.POP.TOTL', date='1960:2010',
+                             value="value", per.page=12000){ 
   require(RJSONIO)
   url <- paste("http://api.worldbank.org/countries/all/indicators/", id,
                "?date=", date, "&format=json&per_page=", per.page,
                sep="")
 
-  wbData = fromJSON(url)[[2]]
+  wbData <- fromJSON(url)[[2]]
    
   wbData = data.frame(
     year = as.numeric(sapply(wbData, "[[", "date")), 
-    value = as.numeric(sapply(wbData, function(x) ifelse(is.null(x[["value"]]),NA, x[["value"]]))), 
+    value = as.numeric(sapply(wbData, function(x)
+      ifelse(is.null(x[["value"]]),NA, x[["value"]]))),  
     country.name = sapply(wbData, function(x) x[["country"]]['value']),
     country.id = sapply(wbData, function(x) x[["country"]]['id'])    
     )
@@ -35,11 +37,12 @@ getWorldBankData <- function(id='SP.POP.TOTL', date='1960:2010', value="value", 
 
 getWorldBankCountries <- function(){
   require(RJSONIO)
-  wbCountries <- fromJSON("http://api.worldbank.org/countries?per_page=12000&format=json")
+  wbCountries <-
+    fromJSON("http://api.worldbank.org/countries?per_page=12000&format=json") 
   wbCountries <- data.frame(t(sapply(wbCountries[[2]], unlist)))
   wbCountries$longitude <- as.numeric(wbCountries$longitude)
   wbCountries$latitude <- as.numeric(wbCountries$latitude)
-  levels(wbCountries$region.value) <- gsub("\\(all income levels\\)",
+  levels(wbCountries$region.value) <- gsub(" \\(all income levels\\)",
                                            "", levels(wbCountries$region.value))
   return(wbCountries)
 }
@@ -61,7 +64,8 @@ population <- getWorldBankData(id='SP.POP.TOTL',  date=years,
 
 ## GDP per capita (current US$)
 GDP.per.capita <- getWorldBankData(id='NY.GDP.PCAP.CD',
-                                   date='1960:2010', value="GDP.per.capita.Current.USD")
+                                   date=years,
+                                   value="GDP.per.capita.Current.USD") 
 
 ## Merge data sets
 wbData <- merge(life.exp, fertility.rate)
@@ -72,12 +76,13 @@ wbData <- merge(wbData, GDP.per.capita)
 wbCountries <- getWorldBankCountries()
 
 ## Add regional information
-wbData <- merge(wbData, wbCountries[c("iso2Code", "region.value",
+wbData <- merge(wbData, wbCountries[c("iso2Code", "region.value", 
                                       "incomeLevel.value")],
                 by.x="country.id", by.y="iso2Code")
 
 ## Filter out the aggregates and country id column
-subData <- subset(wbData, !region.value %in% "Aggregates" , select= -country.id)
+subData <- subset(wbData, !region.value %in% "Aggregates" , select=
+                  -country.id) 
 
 ## Create a motion chart
 M <- gvisMotionChart(subData, idvar="country.name", timevar="year",
