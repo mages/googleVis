fb874bcb12a747d399af2fac10afa3f925ed3861
Markus Gesmann <markus.gesmann@gmail.com>
2014-02-16 09:56:48 +0000

    Added Using Roles via googleVis as a new Markdown vignette.

--- demo/Roles.R
+++ demo/Roles.R
@@ -10,29 +10,32 @@
 # For more details see:
 # https://developers.google.com/chart/interactive/docs/roles
 
+## ---- Tooltip ----
 df <- data.frame(year=1:11,pop=1:11,
                  pop.html.tooltip=letters[1:11])
-
 plot( 
-  gvisScatterChart(df,
-                   options=list(tooltip="{isHtml:'true'}")
-  )
+  gvisScatterChart(df)
 )
 
+## ---- TooltipHTML ----
+levels(df$pop.html.tooltip)[1] <- '<a href="http://www.google.com">Google</a>'
+plot( 
+  gvisScatterChart(df,
+                   options=list(tooltip="{isHtml:'true'}"))
+)
+
+## ---- CertaintyScopeEmphasis ----
 df <- data.frame(year=1:11,pop=1:11,
                  pop.html.tooltip=letters[1:11],
                  y=11:1, y.html.tooltip=LETTERS[11:1],                 
                  y.certainty=c(rep(TRUE, 5), rep(FALSE, 6)),
                  y.scope=c(rep(TRUE, 5), rep(FALSE, 6)),
                  y.emphasis=c(rep(FALSE, 4), rep(TRUE, 7)))
-
 plot(
-  gvisScatterChart(df,
-                   options=list(tooltip="{isHtml:'true'}",              
-                                lineWidth=2))
+  gvisScatterChart(df,options=list(lineWidth=2))
 )
 
-
+## ---- ColumnChart ----
 dat <- data.frame(Year=2010:2013,
                   Sales=c(600, 1500, 800, 1000),
                   Sales.html.tooltip=c('$600K in our first year!',
@@ -41,28 +44,25 @@ dat <- data.frame(Year=2010:2013,
                                        '$1M in sales last year.'),
                   Sales.certainty=c(TRUE, FALSE, TRUE, FALSE),
                   Sales.emphasis=c(TRUE, TRUE, FALSE, FALSE))
-
 plot(
   gvisColumnChart(dat, xvar='Year', 
-                  yvar=c('Sales', 'Sales.certainty'), 
-                  options=list(tooltip="{isHtml:'true'}")
+                  yvar=c('Sales', 'Sales.certainty')
   )
 )
 
+## ---- LimitData ----
 plot(
   gvisLineChart(dat, xvar='Year', 
                 yvar=c('Sales', 'Sales.emphasis', 
-                       'Sales.html.tooltip'),
-                options=list(tooltip="{isHtml:'true'}")
+                       'Sales.html.tooltip')
   )
 )
 
-
+## ---- TwoLines ----
 df <- data.frame(country=c("US", "GB", "BR"), 
                  val1=c(1,3,4), 
                  val1.emphasis=c(TRUE, TRUE, FALSE),
                  val2=c(23,12,32))
-
 plot(
   gvisLineChart(df, xvar="country", 
                 yvar=c("val1", "val1.emphasis")
