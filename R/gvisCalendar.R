f071682bd97abfed9cfea42cdbf886b97baae4bb
Markus Gesmann <markus.gesmann@gmail.com>
2014-03-17 21:12:58 +0000

    Added further examples to gvisCalendar.

--- R/gvisCalendar.R
+++ R/gvisCalendar.R
@@ -79,16 +79,31 @@
 #' cl1 <- gvisCalendar(Cairo, datevar="Date", numvar="Temp")
 #' plot(cl1)
 #'
+#'## Not all months shown? 
+#'## We can change the setting of the width ...
+#' 
 #' cl2 <- gvisCalendar(Cairo, datevar="Date", numvar="Temp",
+#'                     options=list(width=1000))
+#' plot(cl2)
+#' 
+#' ## ... or the cell size
+#' cl3 <- gvisCalendar(Cairo, datevar="Date", 
+#'                     numvar="Temp", 
+#'                     options=list(calendar="{ cellSize: 10 }"))
+#' plot(cl3)
+#' 
+#' ## Example with many options set
+#' cl4 <- gvisCalendar(Cairo, datevar="Date", numvar="Temp",
 #'                     options=list(
 #'                     title="Daily temperature in Cairo",
 #'                     height=500,
 #'                     calendar="{yearLabel: { fontName: 'Times-Roman',
 #'                                fontSize: 32, color: '#1A8763', bold: true},
-#'                     cellColor: { stroke: 'red', strokeOpacity: 0.2 },
-#'                     focusedCellColor: {stroke:'red'}}")
+#'                                cellSize: 10,
+#'                                cellColor: { stroke: 'red', strokeOpacity: 0.2 },
+#'                                focusedCellColor: {stroke:'red'}}")
 #'                   )
-#' plot(cl2)
+#' plot(cl4)
 #'
 
 gvisCalendar <- function(data, datevar="", 
