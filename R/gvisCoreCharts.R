2c08125976a8714e99ca3dbaf6b2bed0ef05c29c
Markus Gesmann <markus.gesmann@gmail.com>
2014-05-05 12:53:32 +0100

    * as submitted to CRAN * added examples for cusomizing points and lines to helpfiles of gvisLineChart and gvisScatterChart

--- R/gvisCoreCharts.R
+++ R/gvisCoreCharts.R
@@ -107,7 +107,16 @@
 #'                        options=list(gvis.editor="Edit me!"))
 #' plot(Line5)
 #' 
-#' 
+#' ## Customizing lines 
+#' Dashed <-  gvisLineChart(df, xvar="country", yvar=c("val1","val2"),
+#'              options=list(
+#'              series="[{color:'green', targetAxisIndex: 0, 
+#'                        lineWidth: 1, lineDashStyle: [2, 2, 20, 2, 20, 2]}, 
+#'                       {color: 'blue',targetAxisIndex: 1, 
+#'                        lineWidth: 2, lineDashStyle: [4, 1]}]",
+#'                        vAxes="[{title:'val1'}, {title:'val2'}]"
+#'                        ))
+#' plot(Dashed)
 #' 
 gvisLineChart <- function(data, xvar="", yvar="", options=list(), 
                           chartid){##, editor
@@ -776,6 +785,25 @@ gvisCandlestickChart<- function(data, xvar="", low="", open="", close="",
 #'                                           pointSize=2))
 #' plot(Scatter4)
 #' 
+#' ## Customize points
+#' M <- matrix(nrow=6,ncol=6)
+#' M[col(M)==row(M)] <- 1:6
+#' dat <- data.frame(X=1:6, M)
+#' SC <- gvisScatterChart(dat, 
+#'                         options=list(
+#'                         title="Customizing points",
+#'                         legend="right",
+#'                         pointSize=30,
+#'                         series="{
+#'                              0: { pointShape: 'circle' },
+#'                              1: { pointShape: 'triangle' },
+#'                              2: { pointShape: 'square' },
+#'                              3: { pointShape: 'diamond' },
+#'                              4: { pointShape: 'star' },
+#'                              5: { pointShape: 'polygon' }
+#'                              }"))
+#' plot(SC)
+#'
 gvisScatterChart <- function(data, options=list(), chartid){
   
   my.type <- "ScatterChart"
