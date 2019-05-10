f4af7c7d344905531e97972acdaf9e6a6bd94593
Ashley Baldry <ashley.baldry@kindredgroup.com>
2019-05-10 18:25:59 +0200

    Include example for explicit word tree

--- R/gvisWordTree.R
+++ R/gvisWordTree.R
@@ -26,9 +26,20 @@
 #' @param data \code{data.frame} that contains the data to be visualised
 #' @param textvar a string that refers to the column name in \code{data} that
 #' contains the text to be used.
-#' @param sizevar a string that refers to the size of the words in the word tree.
-#' @param stylevar a string that refers to the style of the words in the word
-#' tree.
+#' @param sizevar a string that refers to the column name in \code{data} that
+#' contains the size of the words in the word tree.
+#' @param stylevar a string that refers to the column name in \code{data} that
+#' contains the stlye of the text. 
+#' For no warning message in plot, should be called \code{<textvar>.style}.
+#' @param idvar (only when format is explicit) a string that refers to the 
+#' column name in \code{data} that contains the unique ID of the text.
+#' @param parentvar (only when format is explicit) a string that refers to the 
+#' column name in \code{data} that contains the ID of the parent of the text.
+#' @param method a string to say whether the word tree is either:
+#' \itemize{
+#' \item{\code{implicit}}{ - will weight and connect automatically, or} 
+#' \item{\code{explicit}}{ - can specify weights and connections}
+#' }
 #' @param options list of configuration options, see:
 #' 
 #' % START DYNAMIC CONTENT
@@ -82,18 +93,36 @@
 #'                                    wordtree = "{word: 'cats'}",
 #'                                    backgroundColor = "#cba"))
 #' plot(wt2)
-gvisWordTree <- function(data, textvar = "", sizevar = "", stylevar = "", options = list(), chartid) {
+#' 
+#' # Explicit word tree
+#' exp.data <- data.frame(id = as.numeric(0:9),
+#'                        label = letters[1:10],
+#'                        parent = c(-1, 0, 0, 0, 2, 2, 4, 6, 1, 7),
+#'                        size = c(10, 5, 3, 2, 2, 2, 1, 1, 5, 1),
+#'                        stringsAsFactors = FALSE)
+#' 
+#' wt3 <- gvisWordTree(exp.data, idvar = "id", textvar = "label", 
+#'                     parentvar = "parent", sizevar = "size",
+#'                     options = list(wordtree = "{format: 'explicit'}"),
+#'                     method = "explicit")
+#' plot(wt3)
+#' 
+gvisWordTree <- function(data, textvar = "", sizevar = "", stylevar = "", idvar = "", 
+                         parentvar = "", options = list(), method = "implicit", chartid) {
   
   my.type <- "WordTree"
   dataName <- deparse(substitute(data))
   
-  my.options <- list(
-    gvis = modifyList(list(width = 600, height = 500), options),
-    dataName = dataName,
-    data = list(textvar = textvar, sizevar = sizevar, stylevar = stylevar, allowed = c("number", "string"))
-  )
+  if (method == "implicit") {
+    data.list <- list(textvar = textvar, sizevar = sizevar, stylevar = stylevar, allowed = c("number", "string"))
+  } else {
+    data.list <- list(idvar = idvar, textvar = textvar, parentvar = parentvar, sizevar = sizevar,
+                      stylevar = stylevar, allowed = c("number", "string"))
+  }
+  my.options <- list(gvis = modifyList(list(width = 600, height = 500), options),
+                     dataName = dataName, data = data.list)
   
-  checked.data <- gvisCheckWordTreeData(data, my.options)
+  checked.data <- gvisCheckWordTreeData(data, my.options, method)
   
   output <- gvisChart(type = my.type, checked.data = checked.data, options = my.options,
                                   chartid = chartid, package = "wordtree")
@@ -101,12 +130,22 @@ gvisWordTree <- function(data, textvar = "", sizevar = "", stylevar = "", option
   return(output)
 }
 
-gvisCheckWordTreeData <- function(data, options) {
-  data.structure <- list(
-    textvar  = list(mode = "required", FUN = check.char),
-    sizevar  = list(mode = "optional", FUN = check.num.pos),
-    stylevar = list(mode = "optional", FUN = check.char)
-  )
+gvisCheckWordTreeData <- function(data, options, method) {
+  if (method == "explicit") {
+    data.structure <- list(
+      idvar     = list(mode = "required", FUN = check.num),
+      textvar   = list(mode = "required", FUN = check.char),
+      parentvar = list(mode = "required", FUN = check.num),
+      sizevar   = list(mode = "required", FUN = check.num.pos),
+      stylevar  = list(mode = "optional", FUN = check.char)
+    )
+  } else {
+    data.structure <- list(
+      textvar  = list(mode = "required", FUN = check.char),
+      sizevar  = list(mode = "optional", FUN = check.num.pos),
+      stylevar = list(mode = "optional", FUN = check.char)
+    )  
+  }
   
   x <- gvisCheckData(data = data, options = options, data.structure = data.structure)
   
